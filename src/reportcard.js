c6339625c6d22d1aef0d2d94fba31e56f3b0ec51
Clement Lu <lu.705@osu.edu>
2016-04-07 19:07:43 -0400

    Fix reportcard query

--- src/reportcard.js
+++ src/reportcard.js
@@ -5,15 +5,16 @@ chrome.history.search({text: '', maxResults: 10}, function(data) {
     	var td1 = document.createElement('td');
     	var td2 = document.createElement('td');
     	var text1 = document.createTextNode(page.url);
+        var host = getLocation(page.url);
     	var xmlhttp = new XMLHttpRequest();
     	xmlhttp.onreadystatechange = function() {
-   		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        var myArr = JSON.parse(xmlhttp.responseText);
-        var text2 = document.createTextNode(myArr[2]);
-        td2.appendChild(text2);
-   		 }
+   		   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var myArr = JSON.parse(xmlhttp.responseText);
+                var text2 = document.createTextNode(myArr[host.hostname][0][0]);
+                td2.appendChild(text2);
+   		   }
 		};
-    	xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+"www.google.com"+"&key=<API KEY HERE>", true);
+    	xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+host.hostname+"/&key=<API KEY HERE>", true);
     	xmlhttp.send();
 		td1.appendChild(text1);
 		tr.appendChild(td1);
@@ -22,3 +23,9 @@ chrome.history.search({text: '', maxResults: 10}, function(data) {
     });
     document.body.appendChild(table);
 });
+
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
