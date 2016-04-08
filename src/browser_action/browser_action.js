6c88c41c83d23420cb7a97512f570438f5628f2a
Clement Lu <lu.705@osu.edu>
2016-04-07 21:56:00 -0400

    Live WOT results

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -1,12 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
+$(document).ready(function(){
+    var trustworthiness = "Error";
+    var child_safety = "Error";
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs){ //Get active tab
+        var currentTab = tabs[0];
+        var host = getLocation(currentTab.url);
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) { //Wait for results to return
+                var myArr = JSON.parse(xmlhttp.responseText);
+                trustworthiness = myArr[host.hostname]["0"][0];
+                child_safety = myArr[host.hostname]["4"][0];
+                $('#trustworthiness').html(trustworthiness); //Update text in DOM
+                $('#child_safety').html(child_safety);
+            }
+        };
+        xmlhttp.open("GET", "http://api.mywot.com/0.4/public_link_json2?hosts="+host.hostname+"/&key=aac42146ef84f207eda6922c397768d57043c5f5", true);
+        xmlhttp.send();
+        $('#hostname').html(host.hostname);
+    });
 });
+
+var getLocation = function(href) {
+    var l = document.createElement("a");
+    l.href = href;
+    return l;
+};
