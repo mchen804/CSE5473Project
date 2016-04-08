3cb7659b4980db05b478c88750a1d172980f2d65
Clement Lu <lu.705@osu.edu>
2016-04-08 14:12:50 -0400

    Disable alerts for now

--- js/httpsDetection.js
+++ js/httpsDetection.js
@@ -1,6 +1,6 @@
-if(location.protocol == 'http:'){
-	window.alert("http detected");
-}
-if(location.protocol == 'https:'){
-	window.alert("https detected");
-}
+//if(location.protocol == 'http:'){
+//	window.alert("http detected");
+//}
+//if(location.protocol == 'https:'){
+//	window.alert("https detected");
+//}
